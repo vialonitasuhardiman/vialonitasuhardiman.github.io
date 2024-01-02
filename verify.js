5ca1c60ff7ed3eee76f8432dff31011afb0aa581
vialonitasuhardiman <83803329+vialonitasuhardiman@users.noreply.github.com>
2024-01-02 21:34:30 +0800

    Update verify.js

--- verify.js
+++ verify.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('verificationBtn').addEventListener('click', function() {
         var userInput = document.getElementById('verificationInput').value;
         if (userInput === '1551') {
-            window.location.href = 'secret.html';
+            window.location.href = 'En7SgcPRBP5AoFcLpbLzZFoxhYCKUeaDAM7Pr6evZVXhZD5yjmL6dzMM9n2FcMVb.html';
         }
     });
 });
