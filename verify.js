b9bc6a07b7e892984d9050edca0f3ef50a728de8
vialonitasuhardiman <83803329+vialonitasuhardiman@users.noreply.github.com>
2024-01-07 11:27:52 +0700

    Update verify.js

--- verify.js
+++ verify.js
@@ -5,5 +5,8 @@ document.addEventListener('DOMContentLoaded', function() {
         if (userInput === '1551') {
             window.location.href = 'En7SgcPRBP5AoFcLpbLzZFoxhYCKUeaDAM7Pr6evZVXhZD5yjmL6dzMM9n2FcMVb.html';
         }
+        else if (userInput === '0112') {
+            window.location.href = 'yozb5yc3CkjUkGQxvU2KKjKf9vvTT50fRJCRFRxC6ww9nlWtIp1kzP01kSDQ1GEl.html';
+        }
     });
 });
