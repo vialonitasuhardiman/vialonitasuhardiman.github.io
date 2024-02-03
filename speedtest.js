f2d2635f1bdad7d8db6f1aa379991ee85e0b62be
vialonitasuhardiman <83803329+vialonitasuhardiman@users.noreply.github.com>
2024-02-03 15:54:42 +0700

    Update speedtest.js

--- speedtest.js
+++ speedtest.js
@@ -1,26 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const resultText = document.getElementById('result');
+function calculateSpeed() {
+    const startTime = performance.now();
 
-    function calculateSpeed() {
-        const fileSize = 5.24 * 1024 * 1024; // 5MB file size in bytes
-        const startTime = performance.now();
+    fetch('https://erp112.is-a-fullstack.dev/5mb.test') // Replace with a URL to a large file for testing
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.blob(); // Convert the response to a blob
+        })
+        .then(blob => {
+            const endTime = performance.now();
+            const duration = (endTime - startTime) / 1000; // Convert to seconds
+            const downloadSpeed = (blob.size / duration) / 1e6; // MBps
 
-        fetch('https://erp112.is-a-fullstack.dev/5mb.test') // Replace with a URL to a large file for testing
-            .then(response => {
-                const endTime = performance.now();
-                const duration = (endTime - startTime) / 1000; // Convert to seconds
-                const downloadSpeed = (fileSize / duration) / 1e6; // MBps
-
-                resultText.textContent = `Download Speed: ${downloadSpeed.toFixed(2)} Mbps`;
-            })
-            .catch(error => {
-                console.error('Error:', error);
-                resultText.textContent = 'Speed test failed.';
-            });
-    }
-
-    resultText.textContent = 'Click the button below to start the speed test.';
-    
-    const startButton = document.getElementById('startTest');
-    startButton.addEventListener('click', calculateSpeed);
-});
+            resultText.textContent = `Download Speed: ${downloadSpeed.toFixed(2)} Mbps`;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            resultText.textContent = 'Speed test failed.';
+        });
+}
